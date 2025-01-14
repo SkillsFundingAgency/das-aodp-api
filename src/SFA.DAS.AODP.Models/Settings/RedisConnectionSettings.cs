021019832cf57f6f9edde4e10e1d91d49df9a9c1
HamzaIbraheem-DFE <hamza.ibraheem@education.gov.uk>
2025-01-14 09:18:42 +0000

    initial setup (#1)

---
+++ src/SFA.DAS.AODP.Models/Settings/RedisConnectionSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.AODP.Models.Settings
+{
+    public class RedisConnectionSettings
+    {
+        public string RedisConnectionString { get; set; }
+        public string DataProtectionKeysDatabase { get; set; }
+    }
+}
