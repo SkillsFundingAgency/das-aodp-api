b0734427b86e17e909fb549d420c0d1f70647092
JSON Alexander <61407072+jasonalexander-ja@users.noreply.github.com>
2025-01-24 00:13:16 +0000

    Excluded inner exception from the response object.

--- src/SFA.DAS.AODP.Application/BaseResponse.cs
+++ src/SFA.DAS.AODP.Application/BaseResponse.cs
@@ -1,8 +1,11 @@
-﻿namespace SFA.DAS.AODP.Application;
+﻿using System.Text.Json.Serialization;
+
+namespace SFA.DAS.AODP.Application;
 
 public abstract class BaseResponse
 {
     public bool Success { get; set; }
     public string? ErrorMessage { get; set; }
+    [JsonIgnore]
     public Exception? InnerException { get; set; }
 }
