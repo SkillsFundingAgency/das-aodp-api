520628a4314dd0bc7f0fe813997958bfe1698537
peterroddydfe <peter.roddy@education.gov.uk>
2025-01-29 14:28:28 +0000

    Add DB schema for qualifications (#12)

---
+++ src/SFA.DAS.AODP.Database/Tables/FundedQualificationOffer.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[FundedQualificationOffers]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL,
+	[FundedQualificationId] Int NOT NULL,
+	[Name] varchar(255)  NULL,
+	[Notes] varchar(1000)  NULL,
+	[FundingAvailable] BIT NULL,
+	[FundingApprovalStartDate] DATETIME NULL,
+	[FundingApprovalEndDate] DATETIME NULL,
+		
+	CONSTRAINT [PK_FundedQualificationOffers] PRIMARY KEY NONCLUSTERED ( [Id] ASC ),
+	CONSTRAINT [FK_Offers_Qualificats_QualificationId] FOREIGN KEY ([FundedQualificationId]) REFERENCES [FundedQualifications] ([Id]) ON DELETE CASCADE);
