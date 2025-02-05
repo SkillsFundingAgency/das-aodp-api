490551911d4212f40764f9a7f54112c1aa56e025
Tom BEDFORD <Tom.BEDFORD@EDUCATION.GOV.UK>
2025-02-05 16:00:38 +0000

    [AWARD-216] Add Qualification Import Staging table to database project.

---
+++ src/SFA.DAS.AODP.Database/Tables/QualificationImportStaging.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[QualificationImportStaging](
+	[Id] [uniqueidentifier] NOT NULL DEFAULT NEWID(),
+    [JsonData] NVARCHAR(MAX) NULL, 
+    [CreatedDate] DATETIME NULL DEFAULT GETDATE()
+    CONSTRAINT [PK_AwardingOrganisation] PRIMARY KEY ([Id])
+) ON [PRIMARY]
