1b9205f6d1d5155c6e48d21cca02df6244d4f57d
Peter Roddy <Peter.RODDY@EDUCATION.GOV.UK>
2025-02-05 08:44:15 +0000

    CQRS added

---
+++ src/SFA.DAS.AODP.Application/Queries/Qualifications/GetQualificationDetailsQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace SFA.DAS.AODP.Application.Queries.Qualifications
+{
+    public class GetQualificationDetailsQuery : IRequest<GetQualificationDetailsQueryResponse>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ src/SFA.DAS.AODP.Application/Queries/Qualifications/GetQualificationDetailsQueryHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.AODP.Data.Repositories;
+
+namespace SFA.DAS.AODP.Application.Queries.Qualifications
+{
+    public class GetQualificationDetailsQueryHandler : IRequestHandler<GetQualificationDetailsQuery, GetQualificationDetailsQueryResponse>
+    {
+        private readonly INewQualificationsRepository _repository;
+
+        public GetQualificationDetailsQueryHandler(INewQualificationsRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetQualificationDetailsQueryResponse> Handle(GetQualificationDetailsQuery request, CancellationToken cancellationToken)
+        {
+            var qualification = await _repository.GetQualificationDetailsByIdAsync(request.Id);
+            if (qualification == null)
+            {
+                return new GetQualificationDetailsQueryResponse { Success = false };
+            }
+
+            return new GetQualificationDetailsQueryResponse
+            {
+                Success = true,
+                Id = qualification.Id,
+                Status = qualification.Status,
+                Priority = qualification.Priority,
+                Changes = qualification.Changes,
+                QualificationReference = qualification.QualificationReference,
+                AwardingOrganisation = qualification.AwardingOrganisation,
+                Title = qualification.Title,
+                QualificationType = qualification.QualificationType,
+                Level = qualification.Level,
+                ProposedChanges = qualification.ProposedChanges,
+                AgeGroup = qualification.AgeGroup,
+                Category = qualification.Category,
+                Subject = qualification.Subject,
+                SectorSubjectArea = qualification.SectorSubjectArea,
+                Comments = qualification.Comments
+            };
+        }
+    }
+}
