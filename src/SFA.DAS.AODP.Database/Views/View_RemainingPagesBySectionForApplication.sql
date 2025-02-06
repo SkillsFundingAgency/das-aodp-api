219badd9874621c8855e9124977b726be8123e03
HamzaIbraheem-DFE <hamza.ibraheem@education.gov.uk>
2025-02-06 11:20:36 +0000

    wip

--- src/SFA.DAS.AODP.Database/Views/View_RemainingPagesBySectionForApplication.sql
+++ src/SFA.DAS.AODP.Database/Views/View_RemainingPagesBySectionForApplication.sql
@@ -6,6 +6,6 @@ SELECT
 FROM [dbo].[Sections]
 INNER JOIN [dbo].[Applications] ON [dbo].[Applications].[FormVersionId] = [dbo].[Sections].[FormVersionId]
 LEFT JOIN [dbo].[Pages] ON [dbo].[Sections].[Id] = [dbo].[Pages].[SectionId]
-LEFT JOIN [dbo].[ApplicationPages] ON [dbo].[Pages].[Id] = [dbo].[ApplicationPages].[PageId]
+LEFT JOIN [dbo].[ApplicationPages] ON [dbo].[Pages].[Id] = [dbo].[ApplicationPages].[PageId] AND [dbo].[ApplicationPages].[ApplicationId] = [dbo].[Applications].[Id]
 WHERE [dbo].[ApplicationPages].[Id] IS NULL OR [dbo].[ApplicationPages].[Status] = 'NotStarted'
 GROUP BY   [dbo].[Sections].[Id], [dbo].[Applications].[Id]
