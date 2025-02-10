d6f30f4f998615b2f3cc7229470620e60489150b
JSON Alexander <61407072+jasonalexander-ja@users.noreply.github.com>
2025-02-10 15:28:21 +0000

    Added move up and down for form versions.

---
+++ src/SFA.DAS.AODP.Application/Commands/FormBuilder/Forms/MoveFormDownCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using SFA.DAS.AODP.Data.Exceptions;
+using SFA.DAS.AODP.Application.Exceptions;
+using SFA.DAS.AODP.Data.Repositories.FormBuilder;
+
+namespace SFA.DAS.AODP.Application.Commands.FormBuilder.Forms;
+
+public class MoveFormDownCommandHandler : IRequestHandler<MoveFormDownCommand, BaseMediatrResponse<EmptyResponse>>
+{
+    private readonly IFormVersionRepository _formRepository;
+
+    public MoveFormDownCommandHandler(IFormVersionRepository formRepository)
+    {
+        _formRepository = formRepository;
+    }
+
+    public async Task<BaseMediatrResponse<EmptyResponse>> Handle(MoveFormDownCommand request, CancellationToken cancellationToken)
+    {
+        var response = new BaseMediatrResponse<EmptyResponse>();
+        response.Success = false;
+
+        try
+        {
+            var found = await _formRepository.MoveFormVersionOrderDown(request.FormVersionId);
+            response.Success = true;
+        }
+        catch (RecordNotFoundException ex)
+        {
+            response.InnerException = new NotFoundException(ex.Id);
+        }
+        catch (Exception ex)
+        {
+            response.Success = false;
+            response.ErrorMessage = ex.Message;
+        }
+
+        return response;
+    }
+}
---
+++ src/SFA.DAS.AODP.Application/Commands/FormBuilder/Forms/MoveFormUpCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace SFA.DAS.AODP.Application.Commands.FormBuilder.Forms;
+
+public record MoveFormUpCommand(Guid FormVersionId) : IRequest<BaseMediatrResponse<EmptyResponse>>;
