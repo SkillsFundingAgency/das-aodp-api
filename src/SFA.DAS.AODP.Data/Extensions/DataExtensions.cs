975d625e5761826ae1a9c87ac1e40c72d2267330
HamzaIbraheem-DFE <hamza.ibraheem@education.gov.uk>
2025-02-11 22:30:02 +0000

    use MI for db

--- src/SFA.DAS.AODP.Data/Extensions/DataExtensions.cs
+++ src/SFA.DAS.AODP.Data/Extensions/DataExtensions.cs
@@ -1,12 +1,47 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Azure.Core;
+using Azure.Identity;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SFA.DAS.AODP.Data.Context;
 using SFA.DAS.AODP.Data.Repositories.Application;
 using SFA.DAS.AODP.Data.Repositories.FormBuilder;
+using System.Data.Common;
 
 namespace SFA.DAS.AODP.Data.Extensions
 {
+    public static class DatabaseExtensions
+    {
+        private const string AzureResource = "https://database.windows.net/";
+
+        // Take advantage of ChainedTokenCredential's built-in caching
+        private static readonly ChainedTokenCredential AzureServiceTokenProvider = new(
+            new ManagedIdentityCredential(),
+            new AzureCliCredential(),
+            new VisualStudioCodeCredential(),
+            new VisualStudioCredential());
+
+        public static DbConnection GetSqlConnection(string? connectionString)
+        {
+            ArgumentNullException.ThrowIfNull(connectionString);
+
+            var connectionStringBuilder = new SqlConnectionStringBuilder(connectionString);
+            var useManagedIdentity = !connectionStringBuilder.IntegratedSecurity && string.IsNullOrEmpty(connectionStringBuilder.UserID);
+
+            if (!useManagedIdentity)
+            {
+                return new SqlConnection(connectionString);
+            }
+
+            return new SqlConnection
+            {
+                ConnectionString = connectionString,
+                AccessToken = AzureServiceTokenProvider.GetToken(new TokenRequestContext(scopes: [AzureResource])).Token
+            };
+        }
+    }
+
     public static class DataExtensions
     {
         public static IServiceCollection ConfigureDatabase(this IServiceCollection services, IConfigurationRoot configuration)
@@ -23,7 +58,11 @@ namespace SFA.DAS.AODP.Data.Extensions
                 {
                     throw new Exception("Database connection string not found");
                 }
-                options.UseSqlServer(connectionString);
+
+
+                var connection = DatabaseExtensions.GetSqlConnection(connectionString);
+                options.UseSqlServer(connection);
+
             });
             services.AddScoped<IFormVersionRepository, FormVersionRepository>();
             services.AddScoped<ISectionRepository, SectionRepository>();
