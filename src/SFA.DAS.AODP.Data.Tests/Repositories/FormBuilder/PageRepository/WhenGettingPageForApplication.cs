7edd4279fbf6988a661cca1c2c2a69655be7a45e
Olivier Szostak <olivier.szostak@fujitsu.com>
2025-02-19 16:57:06 +0000

    Fix GettingPageForApplication test

--- src/SFA.DAS.AODP.Data.Tests/Repositories/FormBuilder/PageRepository/WhenGettingPageForApplication.cs
+++ src/SFA.DAS.AODP.Data.Tests/Repositories/FormBuilder/PageRepository/WhenGettingPageForApplication.cs
@@ -17,14 +17,22 @@ public class WhenGettingPageForApplication
     public async Task Then_Get_Page_For_Application()
     {
         // Arrange
+        Guid formVersionId = Guid.NewGuid();
         Guid sectionId = Guid.NewGuid();
         Guid pageId = Guid.NewGuid();
+
+        Section newSection = new()
+        {
+            Id = sectionId,
+            FormVersionId = formVersionId
+        };
 
         Page newPage = new()
         {
             Id = pageId,
             SectionId = sectionId,
-            Order = 0
+            Order = 0,
+            Section = newSection
         };
 
         var dbSet = new List<Page>() { newPage };
@@ -32,7 +40,7 @@ public class WhenGettingPageForApplication
         _context.SetupGet(c => c.Pages).ReturnsDbSet(dbSet);
 
         // Act
-        var result = await _sut.GetPageForApplicationAsync(0, sectionId);
+        var result = await _sut.GetPageForApplicationAsync(0, sectionId, formVersionId);
 
         // Assert
         Assert.NotNull(result);
