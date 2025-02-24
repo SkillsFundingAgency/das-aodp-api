3a6db5f99437b42caa8b77c3c956b0cd02648e20
JSON Alexander <61407072+jasonalexander-ja@users.noreply.github.com>
2025-02-24 13:41:08 +0000

    Stashing changes.

---
+++ src/SFA.DAS.AODP.Application.Tests/Commands/Application/Application/CreateApplicationCommandHandlerTests.cs
@@ -0,0 +1,31 @@
+﻿using Moq;
+using SFA.DAS.AODP.Data.Repositories.Application;
+using SFA.DAS.AODP.Data.Repositories.FormBuilder;
+
+namespace SFA.DAS.AODP.Application.Tests.Commands.Application.Application;
+
+public class CreateApplicationCommandHandlerTests
+{
+    private readonly Mock<IApplicationRepository> _applicationRepository = new Mock<IApplicationRepository>();
+    private readonly Mock<IFormVersionRepository> _formVersionRepository = new Mock<IFormVersionRepository>();
+    public CreateApplicationCommandHandler _createApplicationCommandHandler;
+    public CreateApplicationCommandHandlerTests()
+    {
+        _createApplicationCommandHandler
+            = new CreateApplicationCommandHandler(_applicationRepository.Object, _formVersionRepository.Object);
+    }
+
+
+    [Fact]
+    public async Task Test_Application_Created()
+    {
+
+    }
+
+    [Fact]
+    public async Task Test_Inavlid_FormVersionId_Returns_Error()
+    {
+
+    }
+
+}
