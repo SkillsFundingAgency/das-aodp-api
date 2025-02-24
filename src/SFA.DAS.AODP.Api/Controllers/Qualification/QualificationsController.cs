07763d4bbfdc45d5642511f5c16b9e5e0a889891
pete160682 <peterroddy123@live.co.uk>
2025-02-24 08:45:50 +0000

    Get CSV data functionality added to controller. ProducesResponseType attributes added to endpoints

--- src/SFA.DAS.AODP.Api/Controllers/Qualification/QualificationsController.cs
+++ src/SFA.DAS.AODP.Api/Controllers/Qualification/QualificationsController.cs
@@ -1,6 +1,9 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.AODP.Application;
+using SFA.DAS.AODP.Application.Queries.Qualification;
 using SFA.DAS.AODP.Application.Queries.Qualifications;
+using SFA.DAS.AODP.Data.Entities.Qualification;
 
 namespace SFA.DAS.AODP.Api.Controllers.Qualification
 {
@@ -18,21 +21,70 @@ namespace SFA.DAS.AODP.Api.Controllers.Qualification
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetQualifications([FromQuery] string status)
+        [ProducesResponseType(typeof(BaseMediatrResponse<GetNewQualificationsQueryResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetQualifications([FromQuery] string? status)
         {
-            status = status?.Trim().ToLower();
-
-            if (string.IsNullOrEmpty(status))
+            var processedStatus = ProcessAndValidateStatus(status);
+            if (processedStatus is IActionResult badRequestResult)
             {
-                _logger.LogWarning("Qualification status is missing.");
-                return BadRequest(new { message = "Qualification status cannot be empty." });
+                return badRequestResult;
             }
 
-            IActionResult response = status switch
+            IActionResult response = processedStatus switch
             {
                 "new" => await HandleNewQualifications(),
                 // Add more cases for other statuses
-                _ => BadRequest(new { message = $"Invalid status: {status}" })
+                _ => BadRequest(new { message = $"Invalid status: {processedStatus}" })
+            };
+
+            return response;
+        }
+
+        [HttpGet("{qualificationReference}")]
+        [ProducesResponseType(typeof(BaseMediatrResponse<GetQualificationDetailsQueryResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetQualificationDetails(string? qualificationReference)
+        {
+            if (string.IsNullOrWhiteSpace(qualificationReference))
+            {
+                _logger.LogWarning("Qualification reference is empty");
+                return BadRequest(new { message = "Qualification reference cannot be empty" });
+            }
+
+            var result = await _mediator.Send(new GetQualificationDetailsQuery { QualificationReference = qualificationReference });
+
+            if (!result.Success || result.Value == null)
+            {
+                _logger.LogWarning(result.ErrorMessage);
+                return NotFound(new { message = $"No details found for qualification reference: {qualificationReference}" });
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("qualifications/export")]
+        [ProducesResponseType(typeof(BaseMediatrResponse<List<QualificationExport>>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetQualificationCSVExportData([FromQuery] string? status)
+        {
+            var processedStatus = ProcessAndValidateStatus(status);
+            if (processedStatus is IActionResult badRequestResult)
+            {
+                return badRequestResult;
+            }
+
+            IActionResult response = processedStatus switch
+            {
+                "new" => await HandleNewQualificationCSVExport(),
+                // Add more cases for other statuses
+                _ => BadRequest(new { message = $"Invalid status: {processedStatus}" })
             };
 
             return response;
@@ -51,24 +103,33 @@ namespace SFA.DAS.AODP.Api.Controllers.Qualification
             return Ok(result);
         }
 
-        [HttpGet("{qualificationReference}")]
-        public async Task<IActionResult> GetQualificationDetails(string qualificationReference)
+        private async Task<IActionResult> HandleNewQualificationCSVExport()
         {
-            if (string.IsNullOrWhiteSpace(qualificationReference))
-            {
-                _logger.LogWarning("Qualification reference is empty");
-                return BadRequest(new { message = "Qualification reference cannot be empty" });
-            }
+            var result = await _mediator.Send(new GetNewQualificationsCSVExportQuery());
 
-            var result = await _mediator.Send(new GetQualificationDetailsQuery { QualificationReference = qualificationReference });
-
-            if (!result.Success || result.Value == null)
+            if (result == null || !result.Success || result.Value == null)
             {
-                _logger.LogWarning(result.ErrorMessage);
-                return NotFound(new { message = $"No details found for qualification reference: {qualificationReference}" });
+                _logger.LogWarning("No new qualification data found for export.");
+                return NotFound(new { message = "No new qualification data found for export" });
             }
 
             return Ok(result);
         }
+
+        private object ProcessAndValidateStatus(string status)
+        {
+            status = status?.Trim().ToLower();
+
+            if (string.IsNullOrEmpty(status))
+            {
+                _logger.LogWarning("Qualification status is missing.");
+                return BadRequest(new { message = "Qualification status cannot be empty." });
+            }
+
+            return status;
+        }
     }
 }
+
+
+
