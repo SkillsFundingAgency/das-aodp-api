0aa779104d21f4e9c3df28d0885384fdd8b61565
HamzaIbraheem-DFE <hamza.ibraheem@education.gov.uk>
2025-02-25 12:53:27 +0000

    enable swagger outside dev (#58)

--- src/SFA.DAS.AODP.Api/Program.cs
+++ src/SFA.DAS.AODP.Api/Program.cs
@@ -48,12 +48,10 @@ else
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app
     .UseHealthChecks("/ping")
