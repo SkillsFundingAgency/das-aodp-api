8ddecdb3b18534c6d7129a24c2e678bd55e04a40
HamzaIbraheem-DFE <hamza.ibraheem@education.gov.uk>
2025-03-12 12:17:48 +0000

    add message status

--- src/SFA.DAS.AODP.Application/Commands/Application/Message/CreateApplicationMessageCommandHandler.cs
+++ src/SFA.DAS.AODP.Application/Commands/Application/Message/CreateApplicationMessageCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using SFA.DAS.AODP.Data.Entities.Application;
 using SFA.DAS.AODP.Data.Repositories.Application;
 using SFA.DAS.AODP.Models.Application;
 
@@ -6,11 +7,17 @@ namespace SFA.DAS.AODP.Application.Commands.Application.Message;
 
 public class CreateApplicationMessageCommandHandler : IRequestHandler<CreateApplicationMessageCommand, BaseMediatrResponse<CreateApplicationMessageCommandResponse>>
 {
+    private readonly IApplicationReviewRepository _reviewRepository;
+    private readonly IApplicationReviewFeedbackRepository _feedbackRepository;
     private readonly IApplicationMessagesRepository _messageRepository;
+    private readonly IApplicationRepository _applicationRepository;
 
-    public CreateApplicationMessageCommandHandler(IApplicationMessagesRepository messageRepository)
+    public CreateApplicationMessageCommandHandler(IApplicationMessagesRepository messageRepository, IApplicationRepository applicationRepository, IApplicationReviewFeedbackRepository feedbackRepository, IApplicationReviewRepository reviewRepository)
     {
         _messageRepository = messageRepository;
+        _applicationRepository = applicationRepository;
+        _feedbackRepository = feedbackRepository;
+        _reviewRepository = reviewRepository;
     }
 
     public async Task<BaseMediatrResponse<CreateApplicationMessageCommandResponse>> Handle(CreateApplicationMessageCommand request, CancellationToken cancellationToken)
@@ -19,6 +26,9 @@ public class CreateApplicationMessageCommandHandler : IRequestHandler<CreateAppl
 
         try
         {
+            var application = await _applicationRepository.GetByIdAsync(request.ApplicationId);
+            application.UpdatedAt = DateTime.UtcNow;
+
             if (!Enum.TryParse(request.UserType, true, out UserType userType))
                 throw new ArgumentException($"Invalid User Type: {request.UserType}");
 
@@ -46,6 +56,10 @@ public class CreateApplicationMessageCommandHandler : IRequestHandler<CreateAppl
                 SentByEmail = request.SentByEmail
             });
 
+            HandleActionMessages(application, messageType);
+            await HandleReviewUpdate(application.Id, messageTypeConfiguration, messageType);
+            await _applicationRepository.UpdateAsync(application);
+
             response.Value = new() { Id = messageId };
             response.Success = true;
         }
@@ -57,5 +71,65 @@ public class CreateApplicationMessageCommandHandler : IRequestHandler<CreateAppl
         }
 
         return response;
+    }
+
+    private async Task HandleReviewUpdate(Guid applicationId, MessageTypeConfiguration messageTypeConfiguration, MessageType messageType)
+    {
+        var review = await _reviewRepository.GetByApplicationIdAsync(applicationId);
+        if (review == null) return;
+
+        var feedbacksToUpdate = new List<ApplicationReviewFeedback>();
+        foreach (var feedback in review.ApplicationReviewFeedbacks ?? [])
+        {
+            bool updateRequired = false;
+            if (feedback.Type == UserType.Ofqual.ToString() && messageTypeConfiguration.SharedWithOfqual)
+            {
+                updateRequired = true;
+            }
+            else if (feedback.Type == UserType.SkillsEngland.ToString() && messageTypeConfiguration.SharedWithSkillsEngland)
+            {
+                updateRequired = true;
+            }
+            else if (feedback.Type == UserType.Qfau.ToString() && messageTypeConfiguration.SharedWithDfe)
+            {
+                updateRequired = true;
+            }
+
+            if (updateRequired)
+            {
+                feedback.NewMessage = true;
+                feedbacksToUpdate.Add(feedback);
+            }
+        }
+
+        if (messageType == MessageType.PutApplicationOnHold)
+        {
+            var qfauReview = review.ApplicationReviewFeedbacks?.FirstOrDefault(f => f.Type == UserType.Qfau.ToString());
+            if (qfauReview != null)
+            {
+                qfauReview.Status = ApplicationStatus.OnHold.ToString();
+
+                if (!feedbacksToUpdate.Any(f => f.Type == UserType.Qfau.ToString()))
+                {
+                    feedbacksToUpdate.Add(qfauReview);
+                }
+            }
+        }
+
+        await _feedbackRepository.UpdateAsync(feedbacksToUpdate);
+    }
+
+    private static void HandleActionMessages(Data.Entities.Application.Application application, MessageType messageType)
+    {
+        if (messageType == MessageType.UnlockApplication)
+        {
+            application.Status = ApplicationStatus.Draft.ToString();
+            application.Submitted = false;
+            application.SubmittedAt = null;
+        }
+        else if (messageType == MessageType.PutApplicationOnHold)
+        {
+            application.Status = ApplicationStatus.OnHold.ToString();
+        }
     }
 }
