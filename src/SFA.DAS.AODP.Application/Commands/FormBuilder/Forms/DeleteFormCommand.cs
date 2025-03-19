5f6850ffacdb0755d31edebd801c191dd984f280
HamzaIbraheem-DFE <hamza.ibraheem@education.gov.uk>
2025-03-19 13:35:52 +0000

    delete form functionality added

---
+++ src/SFA.DAS.AODP.Application/Commands/FormBuilder/Forms/DeleteFormCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+namespace SFA.DAS.AODP.Application.Commands.FormBuilder.Forms;
+
+public class DeleteFormCommand : IRequest<BaseMediatrResponse<EmptyResponse>>
+{
+    public readonly Guid FormId;
+
+    public DeleteFormCommand(Guid formId)
+    {
+        FormId = formId;
+    }
+}
