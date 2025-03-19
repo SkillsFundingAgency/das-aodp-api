d795157417a7e57a5ff802c1af8e350df6c615d8
HamzaIbraheem-DFE <hamza.ibraheem@education.gov.uk>
2025-03-19 14:29:46 +0000

    delete form functionality added (#89)

--- src/SFA.DAS.AODP.Data/Repositories/FormBuilder/IFormRepository.cs
+++ src/SFA.DAS.AODP.Data/Repositories/FormBuilder/IFormRepository.cs
@@ -3,6 +3,7 @@ namespace SFA.DAS.AODP.Data.Repositories.FormBuilder
 {
     public interface IFormRepository
     {
+        Task Archive(Guid formId);
         int GetMaxOrder();
         Task<bool> MoveFormOrderDown(Guid id);
         Task<bool> MoveFormOrderUp(Guid id);
