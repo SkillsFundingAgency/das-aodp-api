9d4411c8800eacbf5de5eb2e57db56db730b7579
Ravi Udari <95303213+raviudari01@users.noreply.github.com>
2025-04-07 11:52:45 +0100

    Award 669 survey innerapi (#115)
    Award 669 survey inner api (#115)

--- src/SFA.DAS.AODP.Data/Extensions/DataExtensions.cs
+++ src/SFA.DAS.AODP.Data/Extensions/DataExtensions.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SFA.DAS.AODP.Data.Context;
 using SFA.DAS.AODP.Data.Repositories.Application;
+using SFA.DAS.AODP.Data.Repositories.Feedback;
 using SFA.DAS.AODP.Data.Repositories.FormBuilder;
 using SFA.DAS.AODP.Data.Repositories.FundingOffer;
 using SFA.DAS.AODP.Data.Repositories.Jobs;
@@ -44,10 +45,11 @@ namespace SFA.DAS.AODP.Data.Extensions
             services.AddScoped<IQualificationFundingFeedbackRepository, QualificationFundingFeedbackRepository>();
             services.AddScoped<IQualificationFundingsRepository, QualificationFundingsRepository>();
             services.AddScoped<IQualificationDiscussionHistoryRepository, QualificationDiscussionHistoryRepository>();
+            services.AddScoped<IChangedQualificationsRepository, ChangedQualificationsRepository>();
 
             services.AddScoped<IFundingOfferRepository, FundingOfferRepository>();
             services.AddScoped<IApplicationReviewFundingRepository, ApplicationReviewFundingRepository>();
-            services.AddScoped<IChangedQualificationsRepository, ChangedQualificationsRepository>();
+            services.AddScoped<ISurveyRepository, SurveyRepository>();
 
             services.AddScoped<IFundingOfferRepository, FundingOfferRepository>();
             services.AddScoped<IApplicationReviewFundingRepository, ApplicationReviewFundingRepository>();
