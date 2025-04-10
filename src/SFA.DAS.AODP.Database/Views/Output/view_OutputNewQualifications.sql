b30ec4d58dda31f33159695d975913bc93919d56
Robert Rybnikar <robert.rybnikar@education.gov.uk>
2025-04-10 11:47:07 +0100

    AWARD-731

---
+++ src/SFA.DAS.AODP.Database/Views/Output/view_OutputNewQualifications.sql
@@ -0,0 +1,35 @@
+﻿CREATE VIEW [dbo].[view_OutputNewQualifications] AS
+
+/*##################################################################################################
+	-Name:				Output New Qualifications
+	-Description:		All new qualifications that have been funded during the current review cycle
+	-Date of Creation:	10/04/2025
+	-Created By:		Robert Rybnikar
+####################################################################################################*/
+
+SELECT 
+    fq.ImportDate AS DateOfOfqualDataSnapshot,
+	q.QualificationName AS Title,
+	ao.NameOfqual AS OrganisationName,
+    q.Qan AS QualificationNumber,
+	qv.Level AS Level,
+	qv.Type as QualificationType,
+	qv.SubLevel AS Subcategory,
+    qv.SSA AS SectorSubjectArea
+	--foreach offer:
+	--<Name>_FundingAvailable
+	--<Name>_FundingApprovalStartDate
+	--<Name>_FundingApprovalEndDate
+	--<Name>_Notes
+
+FROM dbo.Qualification q
+inner join regulated.QualificationVersions qv ON q.Id = qv.QualificationId
+inner join dbo.AwardingOrganisation ao ON qv.AwardingOrganisationId = ao.Id
+inner join funded.Qualifications fq ON q.Id = fq.QualificationId
+inner join funded.QualificationFundings qualfunding ON qualfunding.QualificationVersionId = qv.Id
+inner join dbo.FundingOffers offertype ON offertype.Id = qualfunding.FundingOfferId
+Left Outer Join regulated.LifecycleStage ls ON qv.LifecycleStageId = ls.Id
+ 
+WHERE ls.Name = 'New'
+	and qv.ProcessStatusId = '00000000-0000-0000-0000-000000000004' --Approved
+GO
