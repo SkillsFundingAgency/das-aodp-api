bf2bbbf9f4e4f0487c503c0787adb6fb019a57b6
Karen Hanna <karen@alveryhanna.com>
2026-01-07 11:56:49 +0000

    removed default valie fro isQanValid

--- src/SFA.DAS.AODP.Application/Commands/Application/Application/EditApplicationCommandResponse.cs
+++ src/SFA.DAS.AODP.Application/Commands/Application/Application/EditApplicationCommandResponse.cs
@@ -1,6 +1,6 @@
 ﻿namespace SFA.DAS.AODP.Application.Commands.Application.Application;
 public class EditApplicationCommandResponse
 {
-    public bool? IsQanValid { get; set; } = false;
+    public bool? IsQanValid { get; set; }
     public string? QanValidationMessage { get; set; }
 }
