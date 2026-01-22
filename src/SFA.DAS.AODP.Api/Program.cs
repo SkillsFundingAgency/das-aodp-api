9698f82c914c7f8c1a9d244a356fd27fcbc05391
Hamzah Shakeel <hamzah.shakeel@education.gov.uk>
2026-01-22 13:30:36 +0000

    added static keyword

--- src/SFA.DAS.AODP.Api/Program.cs
+++ src/SFA.DAS.AODP.Api/Program.cs
@@ -8,7 +8,7 @@ using SFA.DAS.AODP.Application.Swashbuckle;
 namespace SFA.DAS.AODP.Api;
 
 [ExcludeFromCodeCoverage]
-public class Program
+public static class Program
 {
     public static void Main(string[] args)
     {
