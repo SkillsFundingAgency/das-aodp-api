630b8f5ff272c15dd84e79690dce47e6591f02af
AdeelDar-Dfe <Adeel.DAR@education.gov.uk>
2026-01-28 23:33:38 +0000

    Award 901 import defunding list feature (#171)
    * AWARD-901 Import Defunding List
    
    * Made the changes as per the comment in the review.
    
    * Added the Import PLDNS feature
    
    * Refactored the code to resolve issues raised by Sonar
    
    * Refactored the repository
    
    * Moved the common code to a helper class
    
    * Refactored the methods sonar raised the issues against
    
    * Refactored more methods
    
    * Made some more refactoring
    
    * Changed the api route
    
    * Added the import defunding list functionality
    
    * Removed the implementation of the defunding list.
    
    * Added the new data seeding script for the jobs in the postDeployment script.
    
    * Removed as suggested in the code review
    
    * Added the index on Qan field

---
+++ src/SFA.DAS.AODP.Database/MigrationScripts/MVS2_AWARD_901_Jobs_Data_Seed.sql
@@ -0,0 +1,64 @@
+﻿BEGIN TRY
+    BEGIN TRANSACTION
+
+    DECLARE @ScriptName_MVS2_AWARD_901_Jobs_Data_Seed NVARCHAR(100) = 'MVS2_AWARD_901_Jobs_Data_Seed'
+
+    IF EXISTS (SELECT * FROM [MigrationScripts] WHERE [Name] = @ScriptName_MVS2_AWARD_901_Jobs_Data_Seed)
+        BEGIN
+
+            PRINT @ScriptName_MVS2_AWARD_901_Jobs_Data_Seed + ' has already been run.'
+
+        END
+    ELSE
+
+    BEGIN        
+       
+        INSERT INTO [dbo].[Jobs]
+           ([Id]
+           ,[Name]
+           ,[Enabled]
+           ,[Status]
+           ,[LastRunTime])
+        VALUES
+           ('00000000-0000-0000-0000-000000000003'
+           ,'Pldns'
+           ,1
+           ,'Initial'
+           ,null)
+
+        INSERT INTO [dbo].[Jobs]
+           ([Id]
+           ,[Name]
+           ,[Enabled]
+           ,[Status]
+           ,[LastRunTime])
+        VALUES
+           ('00000000-0000-0000-0000-000000000004'
+           ,'DefundingList'
+           ,1
+           ,'Initial'
+           ,null)
+
+        -- Record the script as run
+        INSERT INTO [dbo].[MigrationScripts]
+        (
+            [Name],
+            [RunDate]
+        )
+        VALUES
+        (
+            @ScriptName_MVS2_AWARD_901_Jobs_Data_Seed,
+            GETDATE()
+        )
+    END
+    
+    COMMIT TRANSACTION
+
+END TRY
+BEGIN CATCH
+
+    IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION;
+    
+    THROW
+
+END CATCH
