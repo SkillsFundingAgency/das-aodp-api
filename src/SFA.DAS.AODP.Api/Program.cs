72d91397637db065746299cefb80c97090fd0cf0
hamzahshakeel-DfE <hamzah.shakeel@education.gov.uk>
2026-02-13 09:10:21 +0000

    [AWARD-1084] Added in missing authentication setup (#189)

--- src/SFA.DAS.AODP.Api/Program.cs
+++ src/SFA.DAS.AODP.Api/Program.cs
@@ -1,4 +1,5 @@
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.OpenApi.Models;
 using SFA.DAS.AODP.Api.Extensions;
 using SFA.DAS.AODP.Application.Commands.FormBuilder.Forms;
@@ -16,7 +17,6 @@ public static class Program
 
         var configuration = builder.Configuration.LoadConfiguration(builder.Services, builder.Environment.IsDevelopment());
 
-        // Add services to the container.
         builder.Services
             .AddServiceRegistrations(configuration)
             .AddMediatR(cfg =>
@@ -30,21 +30,46 @@ public static class Program
 
         builder.Services.AddAuthentication(configuration);
 
-        builder.Services.AddControllers();
+        builder.Services.AddControllers(options =>
+        {
+            options.Filters.Add(new AuthorizeFilter());
+        });
 
         builder.Services
             .AddEndpointsApiExplorer()
             .AddSwaggerGen(c =>
             {
-                //Helps with schema's of the same name by adding a number at the end
                 var schemaHelper = new SwashbuckleSchemaHelper();
                 c.CustomSchemaIds(type => schemaHelper.GetSchemaId(type));
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "AODP Inner API", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Paste a JWT here (without the 'Bearer ' prefix)."
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
             });
 
-
         var connectionString = configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"];
-
         if (!string.IsNullOrEmpty(connectionString))
         {
             builder.Services.AddApplicationInsightsTelemetry();
@@ -57,11 +82,11 @@ public static class Program
         app.UseSwagger();
         app.UseSwaggerUI();
 
+        app.UseHealthChecks("/ping");
+        app.UseHttpsRedirection();
 
-        app
-            .UseHealthChecks("/ping")
-            .UseHttpsRedirection()
-            .UseAuthorization();
+        app.UseAuthentication();
+        app.UseAuthorization();
 
         app.MapControllers();
 
