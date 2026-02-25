de35ac95a59ae19f1c4b6e8baaf82038a373ce39
Hamzah Shakeel <hamzah.shakeel@education.gov.uk>
2026-02-25 17:32:15 +0000

    correct scripted

--- src/SFA.DAS.AODP.Database/MigrationScripts/MVS2_AWARD_904_JobsDataSeed.sql
+++ src/SFA.DAS.AODP.Database/MigrationScripts/MVS2_AWARD_904_JobsDataSeed.sql
@@ -33,7 +33,7 @@ INSERT INTO [dbo].[JobConfigurations]
     ,[JobId])
 VALUES
     ('00000000-0000-0000-0000-000000000005'
-        ,'ApiImport'
+        ,'ImportQaaQualifications'
         ,'true'
         ,'00000000-0000-0000-0000-000000000005')
 
