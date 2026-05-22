60bc8c6b803abfc09dc6cdc1eb994c8f26a11817
khanna <karen@laveryhanna.com>
2026-05-22 15:02:32 +0100

    moved p1 checks  into rolloverrun creation

--- src/SFA.DAS.AODP.Application/Commands/Rollover/CreateRolloverWorkflowRunCommandHandler.cs
+++ src/SFA.DAS.AODP.Application/Commands/Rollover/CreateRolloverWorkflowRunCommandHandler.cs
@@ -10,10 +10,12 @@ namespace SFA.DAS.AODP.Application.Commands.Rollover
     : IRequestHandler<CreateRolloverWorkflowRunCommand, BaseMediatrResponse<CreateRolloverWorkflowRunCommandResponse>>
     {
         private readonly IRolloverRepository _repository;
+        private readonly IMediator _mediator;
 
-        public CreateRolloverWorkflowRunCommandHandler(IRolloverRepository rolloverRepository)
+        public CreateRolloverWorkflowRunCommandHandler(IRolloverRepository rolloverRepository, IMediator mediator)
         {
             _repository = rolloverRepository;
+            _mediator = mediator;
         }
 
         public async Task<BaseMediatrResponse<CreateRolloverWorkflowRunCommandResponse>> Handle(CreateRolloverWorkflowRunCommand request, CancellationToken cancellationToken)
