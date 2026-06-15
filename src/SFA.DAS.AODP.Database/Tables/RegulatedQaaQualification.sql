736dae6fe6851219e7591066ac8233fb49ac94d9
Hamzah Shakeel <hamzah.shakeel@education,gov.uk>
2026-06-15 06:30:49 +0100

    updated db columns to nvarchar tomsupport unicode characters more natively

--- src/SFA.DAS.AODP.Database/Tables/RegulatedQaaQualification.sql
+++ src/SFA.DAS.AODP.Database/Tables/RegulatedQaaQualification.sql
@@ -1,16 +1,16 @@
 ﻿CREATE TABLE [regulated].[QaaQualification] (
     [Id] uniqueidentifier NOT NULL,
     [DateOfDataSnapshot] datetime2 NOT NULL,
-    [AimCode] varchar(10) NOT NULL,
-    [QualificationTitle] varchar(250),
-    [AwardingBody] varchar(250) NOT NULL,
-    [Level] [varchar](50) NOT NULL,
-    [Type] [varchar](50) NOT NULL,
-    [Status] [varchar](100) NOT NULL,
+    [AimCode] nvarchar(10) NOT NULL,
+    [QualificationTitle] nvarchar(250),
+    [AwardingBody] nvarchar(250) NOT NULL,
+    [Level] [nvarchar](50) NOT NULL,
+    [Type] [nvarchar](50) NOT NULL,
+    [Status] [nvarchar](100) NOT NULL,
     [StartDate] datetime2 NOT NULL,
     [LastDateForRegistration] datetime2 NOT NULL,
     [LastFundingApprovalEndDate] datetime2 NULL,
-    [SectorSubjectArea] varchar(150) NOT NULL,
+    [SectorSubjectArea] nvarchar(150) NOT NULL,
     CONSTRAINT [PK_QaaQualification] PRIMARY KEY CLUSTERED ([ID] ASC),
     );
 GO
