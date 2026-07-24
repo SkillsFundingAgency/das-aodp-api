6edd93c43cd6381bc435aebcacb70f73dd344a06
Hamzah Shakeel <hamzah.shakeel@education.gov.uk>
2026-07-24 08:11:08 +0100

    tem

--- src/SFA.DAS.AODP.Database/Views/v_ChangedQualificationsExport.sql
+++ src/SFA.DAS.AODP.Database/Views/v_ChangedQualificationsExport.sql
@@ -78,5 +78,4 @@ Left Outer Join regulated.LifecycleStage ls ON qv.LifecycleStageId = ls.Id
 Left Outer Join regulated.ProcessStatus ps ON qv.ProcessStatusId = ps.Id
  
 WHERE ls.Name = 'Changed'
-and (PS.name = 'Decision Required' or PS.Name = 'On Hold')
 GO
